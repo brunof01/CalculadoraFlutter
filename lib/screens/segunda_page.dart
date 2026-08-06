7c57c8058530b60bc826cdefd11642ac8224c296
brunof01 <bruno_e_fortes@estudante.sc.senai.br>
2026-08-05 21:13:55 -0300

    Adicionado novo arquivo

---
+++ lib/screens/segunda_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SegundaPage extends StatefulWidget {
+  @override
+  State<SegundaPage> createState() => _SegundaPageState();
+}
+
+class _SegundaPageState extends State<SegundaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("segunda tela"),
+      ),
+      body: Center(
+        
+          child: Column(
+
+          )
+      ),
+    );
+  }
+}
